a5fd893ceff125a644536c13fb20fa5f6541a9cb
alensochi19 <120686652+alensochi19@users.noreply.github.com>
2023-03-28 19:49:28 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+valid_email = 'alen.sochi19@gmail.com'
+valid_password = 'Alen191919!'
+
+invalid_email = 'alen.sochi19@mail.com'
+invalid_password = 'Alen1919!'
+
+invalid_auth_key = {'key': '45855666248fghhdtythjfjjkdl'}
